5a7533d928d95b854e10f2c2f26deae89ab306d8
epraschifka <edwardpraschifka@gmail.com>
2026-04-27 15:20:57 +0930

    test_bfs.py: update test cases
    - test cases now expect set outputs from get_friends()

--- test_bfs.py
+++ test_bfs.py
@@ -18,61 +18,43 @@ def simulate_api(user_id):
 
     match user_id:
         case "Alice":
-            return [{"steamid": "Bob", "relationship": ..., "friend_since": ...},
-                    {"steamid": "Jones", "relationship": ..., "friend_since": ...},
-                    {"steamid": "Fred", "relationship": ..., "friend_since": ...},]
-        
+            return {"Bob", "Jones", "Fred"}
         case "Bob":
-            return [{"steamid": "Alice", "relationship": ..., "friend_since": ...},
-                    {"steamid": "Alex", "relationship": ..., "friend_since": ...}]
-
+            return {"Alice", "Alex"}
         case "Fred":
-            return [{"steamid": "Alice", "relationship": ..., "friend_since": ...},
-                    {"steamid": "Bill", "relationship": ..., "friend_since": ...}]
-
+            return {"Alice", "Bill"}
         case "Jones":
-            return [{"steamid": "Alice", "relationship": ..., "friend_since": ...},
-                    {"steamid": "Alex", "relationship": ..., "friend_since": ...},
-                    {"steamid": "Dave", "relationship": ..., "friend_since": ...}]
-
+            return {"Alice", "Alex", "Dave"}
         case "Bill":
-            return [{"steamid": "Fred", "relationship": ..., "friend_since": ...}]
-
+            return {"Fred"}
         case "Dave":
-            return [{"steamid": "Jones", "relationship": ..., "friend_since": ...}]
-
+            return {"Jones"}
         case "Alex":
-            return [{"steamid": "Jones", "relationship": ..., "friend_since": ...},
-                    {"steamid": "Bob", "relationship": ..., "friend_since": ...},
-                    {"steamid": "Ron", "relationship": ..., "friend_since": ...}]
-
+            return {"Jones", "Bob", "Ron"}
         case "Ron":
-            return [{"steamid": "Alex", "relationship": ..., "friend_since": ...}]
-        
+            return {"Alex"}
         case _:
             return []
 
 
 def test_bfs_depth_one():
-    out = bfs("Alice", depth=1, process=simulate_api)
-    assert len(out["Alice"]) == 3
-    assert len(out.keys()) == 1
+    id_to_friends = bfs("Alice", depth=1, get_friends_func=simulate_api)
+    assert id_to_friends["Alice"] == {"Bob", "Jones", "Fred"}
     
 def test_bfs_depth_two():
-    out = bfs("Alice", depth=2, process=simulate_api)
-    assert len(out["Fred"]) == 2
-    assert len(out["Jones"]) == 3
-    assert len(out["Bob"]) == 2
-    assert len(out.keys()) == 4
+    id_to_friends = bfs("Alice", depth=2, get_friends_func=simulate_api)
+    assert id_to_friends["Bob"] == {"Alice", "Alex"}
+    assert id_to_friends["Jones"] == {"Alice", "Alex", "Dave"}
+    assert id_to_friends["Fred"] == {"Alice", "Bill"}
+
 
 def test_bfs_depth_three():
-    out = bfs("Alice", depth=3, process=simulate_api)
-    assert len(out["Bill"]) == 1
-    assert len(out["Dave"]) == 1
-    assert len(out["Alex"]) == 3
-    assert len(out.keys()) == 7
+    id_to_friends = bfs("Alice", depth=3, get_friends_func=simulate_api)
+    assert id_to_friends["Bill"] == {"Fred"}
+    assert id_to_friends["Alex"] == {"Jones", "Bob", "Ron"}
+    assert id_to_friends["Dave"] == {"Jones"}
+    
 
 def test_bfs_depth_four():
-    out = bfs("Alice", depth=4, process=simulate_api)
-    assert len(out["Ron"]) == 1
-    assert len(out.keys()) == 8
+    id_to_friends = bfs("Alice", depth=4, get_friends_func=simulate_api)
+    assert id_to_friends["Ron"] == {"Alex"}
--- test_main.py
+++ test_main.py
@@ -3,7 +3,4 @@ from config import USER_ID
 
 def test_get_friends():
     out = get_friends(USER_ID)
-    assert type(out) == list
-    assert "steamid" in out[0].keys() 
-    assert "relationship" in out[0].keys()
-    assert "friend_since" in out[0].keys()
+    assert type(out) == set
