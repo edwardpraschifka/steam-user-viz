e4e6debc8da6c2c6416a8b41b05a4e6aca5e6b81
epraschifka <edwardpraschifka@gmail.com>
2026-04-29 12:57:35 +0930

    improve tests
    - add mock tests
    - add test for no/bad api key

--- test_main.py
+++ test_main.py
@@ -1,6 +1,91 @@
+from unittest.mock import patch
+import requests
+import pytest
+import os
 from main import get_friends
 from config import USER_ID
 
-def test_get_friends():
-    out = get_friends(USER_ID)
-    assert type(out) == set
+@patch("main.requests.get")
+def test_normal(mock_get):
+
+    mock_get.return_value.json.return_value = {
+    "friendslist": {
+        "friends": [
+            {
+                "steamid": "76561197999528143",
+                "relationship": "friend",
+                "friend_since": 1499051558
+            },
+            {
+                "steamid": "76561198047699484",
+                "relationship": "friend",
+                "friend_since": 1453093050
+            }
+        ]
+    }
+    }
+
+    result = get_friends(USER_ID)
+
+    assert result == {
+        "is_private": False,
+        "friends": [
+            {
+                "steamid": "76561197999528143",
+                "relationship": "friend",
+                "friend_since": 1499051558
+            },
+            {
+                "steamid": "76561198047699484",
+                "relationship": "friend",
+                "friend_since": 1453093050
+            }
+        ]
+    }
+
+@patch("main.requests.get")
+def test_private(mock_get):
+
+    mock_get.return_value.status_code = 401
+
+    result = get_friends(USER_ID)
+
+    assert result == {"is_private": True, "friends": []}
+
+@patch("main.requests.get")
+def test_no_friends(mock_get):
+
+    mock_get.return_value.json.return_value = {
+    "friendslist": {
+        "friends": []
+    }
+    }
+
+    result = get_friends(USER_ID)
+
+    assert result == {"is_private": False, "friends": []}
+
+@patch("main.requests.get")                                                   
+def test_invalid_user(mock_get):
+    mock_get.return_value.ok = True                                           
+    mock_get.return_value.status_code = 400
+    mock_get.return_value.raise_for_status.side_effect = requests.exceptions.HTTPError("400")
+                                                                            
+    with pytest.raises(RuntimeError):
+        get_friends("invalid user id")
+
+@patch("main.requests.get")                                                   
+def test_bad_api_key(mock_get):
+    mock_get.return_value.ok = True                                           
+    mock_get.return_value.status_code = 400
+    mock_get.return_value.raise_for_status.side_effect = requests.exceptions.HTTPError("400")
+                                                                            
+    with pytest.raises(RuntimeError):
+        get_friends(USER_ID)
+
+@patch("main.requests.get")
+def test_timeout(mock_get):
+    mock_get.side_effect = requests.exceptions.Timeout                        
+
+    with pytest.raises(RuntimeError):                                         
+        get_friends(USER_ID)
