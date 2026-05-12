6ce2a50c9a90e7ef6b49ece860758e51971c090b
epraschifka <edwardpraschifka@gmail.com>
2026-05-12 13:19:46 +0930

    Update: graph data structure
    - Change vanilla class -> dataclass
    - dataclass automatically does serialization

--- app/graph.py
+++ app/graph.py
@@ -1,10 +1,30 @@
+from dataclasses import dataclass, field, asdict
+import json
+
+@dataclass
+class Node:
+    id: str
+    name: str
+    profileurl: str
+    avatarurl: str
+
+@dataclass
+class Link:
+    source: str
+    target: str
+
+@dataclass
 class Graph:
-    def __init__(self):
-       self.data = {}
-    
-    def add(self, data):
-        """adds data from a new dictionary to the graph"""
-        
-        self.data = self.data | data
+    nodes: list[Node] = field(default_factory=list)
+    links: list[Link] = field(default_factory=list)
 
+    def add_node(self, id: str, name: str, profileurl: str, avatarurl: str) -> None:
+        self.nodes.append(Node(id=id, name=name, profileurl=profileurl, avatarurl=avatarurl))
 
+    def add_link(self, source: str, target: str) -> None:
+        self.links.append(Link(source=source, target=target))
+
+    def serialize(self) -> dict:
+        return asdict(self)
+
+graph = Graph()
