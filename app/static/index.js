7f1378e483d3d9d8f16b21b3e7dd7320a3b526d6
epraschifka <edwardpraschifka@gmail.com>
2026-05-13 12:20:36 +0930

    Update: graph expands upon node click
    Clicking a node causes the node's friends
    to be added to the graph.

--- app/static/index.js
+++ app/static/index.js
@@ -2,30 +2,56 @@ import * as THREE from 'https://esm.sh/three';
 
 const sessionId = crypto.randomUUID();
 
-async function handleSubmit() {
-const id = document.getElementById("id").value;
+const Graph = new ForceGraph3D(document.getElementById('3d-graph'))
+            .nodeThreeObject(({ avatarurl }) => {
+                const imgTexture = new THREE.TextureLoader().load(avatarurl);
+                imgTexture.colorSpace = THREE.SRGBColorSpace;
+                const material = new THREE.SpriteMaterial({ map: imgTexture });
+                const sprite = new THREE.Sprite(material);
+                sprite.scale.set(12, 12);
+                return sprite;
+            })
+            .onNodeClick(node => {
+                console.log(node.id)
+                handleExpand(node.id)
+            })
 
-const response = await fetch("/graph", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ id, session_id: sessionId, skip_self: 0 })
-});
 
-const gData = await response.json();
-
-if (response.ok && gData["success"]) {
-    console.log(gData["data"])
-    const Graph = new ForceGraph3D(document.getElementById('3d-graph'))
-        .nodeThreeObject(({ avatarurl }) => {
-            const imgTexture = new THREE.TextureLoader().load(avatarurl);
-            imgTexture.colorSpace = THREE.SRGBColorSpace;
-            const material = new THREE.SpriteMaterial({ map: imgTexture });
-            const sprite = new THREE.Sprite(material);
-            sprite.scale.set(12, 12);
-            return sprite;
-        })
-        .graphData(gData["data"]);
+async function renderGraph(gData) {
+    Graph.graphData(gData["data"])
 }
+
+async function handleSubmit() {
+    const id = document.getElementById("id").value;
+
+    const response = await fetch("/graph", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id, session_id: sessionId, skip_self: 0 })
+    });
+
+    const gData = await response.json();
+
+    if (response.ok && gData["success"]) {
+        renderGraph(gData)
+    }
+}
+
+async function handleExpand(id) {
+    const response = await fetch("/graph", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id, session_id: sessionId, skip_self: 0 })
+    });
+
+    const gData = await response.json();
+
+    console.log(gData)
+    
+    
+    if (response.ok && gData["success"]) {
+        renderGraph(gData)
+    }
 }
 
 document.getElementById("submit-btn").addEventListener("click", handleSubmit);
